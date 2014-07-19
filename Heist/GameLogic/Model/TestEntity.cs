8946f9495440ffd87352356d2646ad3d24444377
jackmacattack <jackmacattack92@gmail.com>
2014-07-18 22:26:48 -0400

    Added hitbox

--- Heist/GameLogic/Model/TestEntity.cs
+++ Heist/GameLogic/Model/TestEntity.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,11 @@ namespace GameLogic.Model
         public TestEntity(string texName, int id, double x, double y, double accel)
             : base(texName, id, x, y, accel)
         {
+        }
+
+        public override Rectangle GetHitBox()
+        {
+            return new Rectangle((int)x, (int)y, 64, 64);
         }
     }
 }
